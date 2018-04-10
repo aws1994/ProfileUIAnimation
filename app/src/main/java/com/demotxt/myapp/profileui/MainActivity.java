043742a6d5b9bcff56d83592314eb184d306fae5
aws1994 <awsrhimi@gmail.com>
2018-04-10 14:07:50 +0100

    Initial commit

---
+++ app/src/main/java/com/demotxt/myapp/profileui/MainActivity.java
@@ -0,0 +1,69 @@
+package com.demotxt.myapp.profileui;
+
+import android.support.constraint.ConstraintLayout;
+import android.support.constraint.ConstraintSet;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.transition.TransitionManager;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private boolean isOpen = false ;
+    private ConstraintSet layout1,layout2;
+    private ConstraintLayout constraintLayout ;
+    private ImageView imageViewPhoto;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // changing the status bar color to transparent
+
+        Window w = getWindow();
+        w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+
+        layout1 = new ConstraintSet();
+        layout2 = new ConstraintSet();
+        imageViewPhoto = findViewById(R.id.photo);
+        constraintLayout = findViewById(R.id.constraint_layout);
+        layout2.clone(this,R.layout.profile_expanded);
+        layout1.clone(constraintLayout);
+
+        imageViewPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                if (!isOpen) {
+                    TransitionManager.beginDelayedTransition(constraintLayout);
+                    layout2.applyTo(constraintLayout);
+                    isOpen = !isOpen ;
+                }
+
+                else {
+
+                    TransitionManager.beginDelayedTransition(constraintLayout);
+                    layout1.applyTo(constraintLayout);
+                    isOpen = !isOpen ;
+
+                }
+
+
+
+
+
+
+            }
+        });
+
+
+
+
+    }
+}
